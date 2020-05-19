e352d3b53ed73b103e60c4cdb1bf075a0c1f951b
ethan.zammit.19 <ethan.zammit.19@um.edu.mt>
2020-05-19 23:25:03 +0200

    notes for future

--- parser.cpp
+++ parser.cpp
@@ -2,3 +2,8 @@
 
 //given input, this class will determine some attributes of input such as pipes, redirects etc
 // then the respective handler is called
+int classify(char *input){
+    //check for pipes
+    //check for redirection
+    //strtok was recommended, check what it does !
+}
