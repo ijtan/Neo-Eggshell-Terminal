bb3d07c1e10997996668ad7e54849ce010e6be34
Ethan Zammit <ethan.zammit.19@um.edu.mt>
2020-05-19 19:47:00 +0200

    further testing fork pid returns

--- external.cpp
+++ external.cpp
@@ -1,20 +1,24 @@
 #include <cstdio>
 #include "unistd.h"
 #include "external.h"
-
+using namespace std;
 void runExt(char * exec){
     pid_t pid = fork();
     printf("%d\n",pid);
     printf("GET %d\n",getpid());
 
     printf("-------------------\n");
-    if(pid == 0){
-        std::cout << pid  << ": exiting\n";
-        exit(0);
+//    if(pid == 0){
+//        std::cout << pid  << ": exiting\n";
+//        exit(0);
+//    }
+    if(pid == 0) {
+        puts("executing");
+        char *arg[2];
+        arg[0] = "/bin/ls";
+        cout << execl("/bin/ls","ls", NULL)<< ": execv\n";
     }
-
-    char *arg[2];
-    arg[0] = "/bin/ls";
-    arg[1] = NULL;
-    execv(arg[0], arg);
+    puts("Done");
+    std::cout << pid  << " EXITING";
+    sleep(2);
 }
