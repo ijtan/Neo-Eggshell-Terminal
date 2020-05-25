1c860d0a23a900d6a8189ac5f3dfcdc3ce5b9f8b
Ethan <ethanzammit@gmail.com>
2020-05-26 01:02:09 +0200

    i committed from other account whoops

--- internal.h
+++ internal.h
@@ -1,7 +1,3 @@
-//
-// Created on 5/20/2020.
-//
-
 #ifndef NEOEGGSHELL_INTERNAL_H
 #define NEOEGGSHELL_INTERNAL_H
 
@@ -22,9 +18,7 @@
 using namespace std;
 int internalHandler(string command, vector<string> argsV);
 void echo(vector<string>args);
-//int set(vector<string> args);
 int better_set(string variable,string value);
-//void unset(vector<string> args);
 void better_unset(string variable);
 void changeDirs(vector<string> args);
 int sourceStart(vector<string> args);
