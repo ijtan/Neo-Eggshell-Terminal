4f02c614de6bf9a7b44cedd81a8d9b319b4d6d29
Ethan Zammit <ethan.zammit.19@um.edu.mt>
2020-06-21 20:34:44 +0200

    minor var change

--- variableHandler.cpp
+++ variableHandler.cpp
@@ -11,34 +11,32 @@ void printVars() {
 void parseVars(vector<string> &input, string &line) {
     int i = 0;
     for (auto in : input) {
-        if (in.find('$') == string::npos) {
-            i++;
-            continue;
-        }
-        if (in.find('$') > 0) {
-            if (in[in.find('$') - 1] == '\\') {
-                string escaped = in.erase(in.find('$') - 1, 1);
-                input[i] = escaped;
-                i++;
-                continue;
+        if (in.find('$') != string::npos) {
+            if (in.find('$') > 0) {
+                if (in[in.find('$') - 1] == '\\') {
+                    string escaped = in.erase(in.find('$') - 1, 1);
+                    input[i] = escaped;
+                    i++;
+                    continue;
+                }
             }
+            string envName = in.substr(in.find('$') + 1);
+            if (getenv(envName.c_str()) != NULL) {
+                string newEnv = in.substr(0, in.find('$'));
+                newEnv.append(getenv(envName.c_str()));
+                input[i] = newEnv;
+
+                int pos = line.find('$');
+                int j;
+                for (j = pos; j < line.length(); j++)
+                    if (line[j] == ' ')
+                        break;
+
+                line.erase(pos, j - pos);
+                line.insert(pos, getenv(envName.c_str()));
+            } else
+                cout << "The environment for variable '" << envName.c_str() << "' was not found!" << endl;
         }
-        string envName = in.substr(in.find('$') + 1, in.length());
-        if (getenv(envName.c_str()) != NULL) {
-            string newEnv = in.substr(0, in.find('$'));
-            newEnv.append(getenv(envName.c_str()));
-            input[i] = newEnv;
-
-            int pos = line.find('$');
-            int j;
-            for (j = pos; j < line.length(); j++)
-                if (line[j] == ' ')
-                    break;
-
-            line.erase(pos, j);
-            line.insert(pos, getenv(envName.c_str()));
-        } else
-            cout << "The environment for variable '" << envName.c_str() << "' was not found!" << endl;
         i++;
     }
 }
