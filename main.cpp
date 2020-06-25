d7463359cd31359608aa8a63eeae31308a1eb2ca
Ethan Zammit <ethan.zammit.19@um.edu.mt>
2020-06-25 17:08:30 +0200

    trying colours

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ void lineReadInit() {
     linenoiseHistoryLoad(shell.c_str());
     pid_t callerID = getppid();
     //start linenoise loop
+
     while ((line = linenoise(getenv("PROMPT"))) != NULL) {
         linenoiseHistoryAdd(line);
         linenoiseHistorySave(shell.c_str());
